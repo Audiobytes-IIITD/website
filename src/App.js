1052f5cef89d78e163c024f52a8c10ad6b262942
thatspiyush08 <piyushnaula008@gmail.com>
2024-03-08 21:11:35 +0530

    added navbar route with its code

--- src/App.js
+++ src/App.js
@@ -5,12 +5,14 @@ import About from './components/about';
 import Home from './components/home';
 import Join from './components/join';
 import Members from './components/members';
+import NavBar from './components/navbar'
 
 
 function App() {
   return (
     <div className="App">
       <Home />
+      <NavBar/>
       <About/>
       <Members/>
       <Join/>
