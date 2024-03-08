735e3e0bb67fd9a0328786dd800fb63627160392
thatspiyush08 <piyushnaula008@gmail.com>
2024-03-08 20:01:14 +0530

    added routes

---
+++ src/components/home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const home=()=>{
+    return(
+        <div>
+            <h1>Home</h1>
+            <p>Welcome to the home page</p>
+        </div>
+    )
+};
+
+export default home;
