9a2504d9f9ae7a6a2e6e8223759dd239b45df80a
HarshAggarwal1 <haggarwal2510@gmail.com>
2024-03-09 11:55:42 +0530

    Added Members Page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import NavBar from './components/NavBar';
 import Join from './pages/Join';
+import Members from './pages/Members';
 
 function App() {
   return (
     <div className="App">
       <NavBar />
       <div className="mt-16">
+        <Members />
         <Join />
       </div>
     </div>
