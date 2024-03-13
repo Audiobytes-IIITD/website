432f9d224f4967195a2d0718d70e4c1c8d85e19c
Anish <132756021+ianishdev@users.noreply.github.com>
2024-03-13 11:26:36 +0530

    Update Members.js

--- src/pages/Members/Members.js
+++ src/pages/Members/Members.js
@@ -6,17 +6,21 @@ import image from '../../images/man.png';
 function Members() {
     return (
         <div className="members-container">
+            <h1 className="members-title" id="title">Members Profile</h1>
             <div className="members-content">
-                <h1 className="members-title" id="heading">Members Profile</h1>
-
-                <MemberCard name="Harsh Aggarwal" role="Web Developer" email="harsh20508@iiitd.ac.in" image={image} />
                 <MemberCard name="Anish" role="Web Developer" email="anish22075@iiitd.ac.in" image={image} />
+                <MemberCard name="Harsh Aggarwal" role="Web Developer" email="harsh20508@iiitd.ac.in" image={image} />
                 <MemberCard name="Piyush Narula" role="Web Developer" email="piyush22354@iiitd.ac.in" image={image} />
-
+                <MemberCard name="Anish" role="Web Developer" email="anish22075@iiitd.ac.in" image={image} />
+                <MemberCard name="Harsh Aggarwal" role="Web Developer" email="harsh20508@iiitd.ac.in" image={image} />
+                <MemberCard name="Piyush Narula" role="Web Developer" email="piyush22354@iiitd.ac.in" image={image} />
+                <MemberCard name="Anish" role="Web Developer" email="anish22075@iiitd.ac.in" image={image} />
+                <MemberCard name="Harsh Aggarwal" role="Web Developer" email="harsh20508@iiitd.ac.in" image={image} />
+                <MemberCard name="Piyush Narula" role="Web Developer" email="piyush22354@iiitd.ac.in" image={image} />
+                <MemberCard name="Piyush Narula" role="Web Developer" email="piyush22354@iiitd.ac.in" image={image} />
             </div>
         </div>
     );
 };
 
 export default Members;
-
