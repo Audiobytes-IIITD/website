85420906b632e3c406ac5083f3b35cebdf2a67bc
thatspiyush08 <piyushnaula008@gmail.com>
2024-03-13 15:56:26 +0530

    home page line size increased

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -9,7 +9,7 @@ const Home = () => {
         <div className="flex flex-col gap-10">
           <div className="">
             <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-center text-black">
-              <h1 className="text-2xl md:text-5xl font-bold mb-4" style={{ color: 'white', fontSize: '4.5rem' }}>Welcome to AudioBytes</h1>
+              <h1 className="text-2xl md:text-5xl font-bold mb-4" style={{ color: 'white', fontSize: '7.5rem' }}>Welcome to AudioBytes</h1>
               <h2 className="text-sm md:text-xl" style={{ color: 'white', fontSize: '2rem', marginTop: '20px' }}>MUSIC CLUB OF IIITD</h2>
             </div>
           </div>
