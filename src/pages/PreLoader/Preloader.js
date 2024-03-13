22c659b5de5f898527f3c421f52217254b7495ab
Anish <132756021+ianishdev@users.noreply.github.com>
2024-03-13 11:28:15 +0530

    Add files via upload

---
+++ src/pages/PreLoader/Preloader.js
@@ -0,0 +1,27 @@
+import React from "react";
+import "./Preloader.css";
+
+const Preloader = () => {
+
+    
+
+
+    return (
+        <div className="container">
+            <div class="music">
+            <div class="bar"></div>
+            <div class="bar"></div>
+            <div class="bar"></div>
+            <div class="bar"></div>
+            <div class="bar"></div>
+            <div class="bar"></div>
+            <div class="bar"></div>
+            <div class="bar"></div>
+            <div class="bar"></div>
+            <div class="bar"></div>
+            </div>
+        </div>
+    )
+};
+
+export default Preloader;
