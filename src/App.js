09798f0139357f473dd1dd2366ee59bbc2122492
Anish <132756021+ianishdev@users.noreply.github.com>
2024-03-13 06:59:13 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import './App.css';
 import NavBar from './components/NavBar';
+import Join from '../src/pages/Join/join';
+import Members from '../src/pages/Members/Members';
+import About from '../src/pages/About/About';
+import Home from '../src/pages/Home/Home';
 
 function App() {
   return (
     <div className="App">
       <NavBar />
+      <div className="mt-16">
+        <Home />
+        <About />
+        <Members />
+        <Join />
+      </div>
     </div>
   );
 }
