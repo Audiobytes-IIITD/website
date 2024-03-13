ce922d57cbafa29ba43d4c70c7a2aba5f2393ead
Anish <132756021+ianishdev@users.noreply.github.com>
2024-03-13 11:22:58 +0530

    Update NavBar.js

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,7 +2,6 @@ import React from "react";
 import logo from "../images/abWhite300.png";
 
 function NavBar() {
-    const [showMenu, setShowMenu] = React.useState(false);
     
     return (
         <nav className="bg-black dark:bg-gray-900 fixed w-full z-20 top-0 start-0 border-b border-gray-200 dark:border-gray-600">
