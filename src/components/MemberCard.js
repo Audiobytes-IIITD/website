b8d1dee6ddb2520c62b40198a4b3733093b778bd
Anish <132756021+ianishdev@users.noreply.github.com>
2024-03-13 12:59:24 +0530

    Update MemberCard.js

--- src/components/MemberCard.js
+++ src/components/MemberCard.js
@@ -2,10 +2,10 @@ import React from "react";
 
 const MemberCard = ({ name, role, email, image }) => {
     return (
-        <div className="flex items-center gap-4 bg-slate-800 p-4 rounded-xl flex-wrap xs:justify-center sm:justify-center xxs:justify-center">
+        <div className="flex items-center gap-4 bg-black-800 p-4 rounded-xl flex-wrap xs:justify-center sm:justify-center xxs:justify-center" style={{ width: "300px", height: "150px", margin: "10px", flexBasis: "calc(25% - 20px)" }}> {/* Added flexBasis for controlling width */}
             <img src={image} alt="member" className="w-32 h-32 rounded-full" />
             <div className="flex flex-col gap-2">
-                <h1 className="text-white text-lg font-bold">{name}</h1>
+                <h2 className="text-white text-lg font-bold">{name}</h2>
                 <p className="text-white text-sm">{role}</p>
                 <p className="text-white text-sm">{email}</p>
             </div>
