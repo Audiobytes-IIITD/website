38e2cc02a6d334ea06c44b2e142aa3e258ea99a1
Anish <132756021+ianishdev@users.noreply.github.com>
2024-03-13 12:59:42 +0530

    Update NavBar.js

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,58 +1,41 @@
 import React from "react";
-import { Link } from "react-scroll";
-
+import logo from "../images/abWhite300.png";
 
 function NavBar() {
-    const [showMenu, setShowMenu] = React.useState(false);
+    
     return (
+        <nav className="bg-black dark:bg-gray-900 fixed w-full z-20 top-0 start-0 border-b border-gray-200 dark:border-gray-600">
+            <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+                <div className="flex items-center space-x-3 rtl:space-x-reverse">
+                    <img src={logo} alt="icon" className="w-16 h-16 content-stretch" />
+                    <h1 className="text-2xl font-semibold whitespace-nowrap dark:text-white" style={{ color: 'white' }}>AudioBytes</h1>
+                </div>
+                <div className="flex md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
+                    <button type="button" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Join Us</button>
+                    <button data-collapse-toggle="navbar-sticky" type="button" className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-sticky" aria-expanded="false">
+                        <span className="sr-only">Open main menu</span>
+                        <svg className="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
+                            <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M1 1h15M1 7h15M1 13h15"/>
+                        </svg>
+                    </button>
+                </div>
 
-        <nav className="fixed top-0 left-0 right-0 flex items-center justify-between bg-slate-800 p-4 rounded-ee-xl rounded-es-xl z-10 shadow-sm min-w-80">
-            <div className="flex items-center">
-                <img src="images/abWhite300.png" alt="icon" className="w-16 h-16 content-stretch" />
-                <h1 className="text-white font-bold text-base">AudioBytes</h1>
-            </div>
-            <div className="xs:hidden sm:hidden md:flex lg:flex items-center xxs:hidden">
-                {/* Content for large screens */}
 
-                <Link to='aboutUs' activeClass='active' style={{ cursor: 'pointer' }} spy={true} smooth={true}>
-                    <div className="text-white text-sm font-bold mr-8" onClick={() => { setShowMenu(!showMenu) }}>About Us</div>
-                </Link>
-                <a href="/" className="text-white mr-8 text-sm font-bold">Upcoming Events</a>
-                <Link to='members' activeClass='active' style={{ cursor: 'pointer' }} spy={true} smooth={true}>
-                    <div className="text-white mr-8 text-sm font-bold">Our Members</div>
-                </Link>
-
-                <Link to='members' activeClass='active' style={{ cursor: 'pointer' }} spy={true} smooth={true}>
-                    <div className="text-white mr-8 text-sm font-bold">Contact</div>
-                </Link>
-
-                <Link to='joinus' activeClass='active' style={{ cursor: 'pointer' }} spy={true} smooth={true}>
-                    <div className="text-white mr-4 text-sm rounded-full bg-cyan-600 p-3 font-bold">Join the Club!</div>
-                </Link>
-
-            </div>
-            <div className="lg:hidden md:hidden">
-                {/* Content for small screens */}
-                <button className="text-white text-sm font-bold bg-cyan-600 rounded-full p-3" onClick={() => { setShowMenu(!showMenu) }}>Menu</button>
-                <div className={` opacity-95 flex flex-col fixed top-0 left-0 w-full h-full items-center justify-center bg-slate-800 z-50 ${showMenu ? "block" : "hidden"}`}>
-                    <img src="images/close.png" alt="close_menu" className="mt-2 hover:cursor-pointer w-6 h-6 content-between" onClick={() => setShowMenu(false)} />
-                    <div className="flex flex-col items-center justify-center h-full gap-5 p-2">
-                        <Link to='aboutUs' activeClass='active' style={{ cursor: 'pointer' }} spy={true} smooth={true}>
-                            <div className="text-white text-sm font-bold mr-8" onClick={() => { setShowMenu(!showMenu) }}>About Us</div>
-                        </Link>
-                        <a href="/" className="text-white text-sm font-bold" onClick={() => { setShowMenu(!showMenu) }}>Upcoming Events</a>
-                        <Link to='members' activeClass='active' style={{ cursor: 'pointer' }} spy={true} smooth={true}>
-                            <div className="text-white text-sm font-bold" onClick={() => { setShowMenu(!showMenu) }}>Our Members</div>
-                        </Link>
-                        <Link to='members' activeClass='active' style={{ cursor: 'pointer' }} spy={true} smooth={true}>
-                            <div className="text-white text-sm font-bold" onClick={() => { setShowMenu(!showMenu) }}>Contact</div>
-                        </Link>
-
-                        <Link to='joinus' activeClass='active' style={{ cursor: 'pointer' }} spy={true} smooth={true}>
-                            <div className="text-white text-sm font-bold" onClick={() => { setShowMenu(!showMenu) }}>Join the Club!</div>
-                        </Link>
-
-                    </div>
+                <div className="items-center justify-between hidden w-full md:flex md:w-auto md:order-1" id="navbar-sticky">
+                    <ul className="flex flex-col p-4 md:p-0 mt-4 font-medium border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-black dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+                        <li>
+                            <a href="#" className="block py-2 px-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 md:dark:text-blue-500" aria-current="page" style={{ color: 'white' }}>Home</a>
+                        </li>
+                        <li>
+                            <a href="#" className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700" style={{ color: 'white' }}>About</a>
+                        </li>
+                        <li>
+                            <a href="#" className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700" style={{ color: 'white' }}>Upcoming Events</a>
+                        </li>
+                        <li>
+                            <a href="#" className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700" >Contact</a>
+                        </li>
+                    </ul>
                 </div>
             </div>
         </nav>
