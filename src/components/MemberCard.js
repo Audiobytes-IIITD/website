4e37ac5e543c56d4b929ec0fb6e1a5125a0250e0
Anish <132756021+ianishdev@users.noreply.github.com>
2024-03-13 06:58:32 +0530

    Add files via upload

---
+++ src/components/MemberCard.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const MemberCard = ({ name, role, email, image }) => {
+    return (
+        <div className="flex items-center gap-4 bg-black-800 p-4 rounded-xl flex-wrap xs:justify-center sm:justify-center xxs:justify-center" style={{ width: "300px", height: "150px" }}>
+            <img src={image} alt="member" className="w-32 h-32 rounded-full" />
+            <div className="flex flex-col gap-2">
+                <h2 className="text-white text-lg font-bold">{name}</h2>
+                <p className="text-white text-sm">{role}</p>
+                <p className="text-white text-sm">{email}</p>
+            </div>
+        </div>
+    );
+};
+
+export default MemberCard;
