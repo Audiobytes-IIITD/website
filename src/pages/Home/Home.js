0b34d4f7f1750a0ba0086a94f66c1027a94a8f59
thatspiyush08 <piyushnaula008@gmail.com>
2024-03-13 18:54:17 +0530

    added animation

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,21 +1,48 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import "./Home.css";
 import video from '../../videos/vid.mp4';
+import Typed from 'typed.js';
+import { gsap } from 'gsap/all'; 
+import { ScrollTrigger } from 'gsap/all'; 
+import SplitType from 'split-type';
 
 const Home = () => {
+  useEffect(() => {
+    
+    var typing = new Typed(".text", {
+      strings: [" Vibe", " Perform.", " Muse."],
+      typeSpeed: 100,
+      backSpeed: 40,
+      loop: true
+    });
+
+    
+    gsap.registerPlugin(ScrollTrigger);
+    const introsplitTypes = document.querySelectorAll(".left-part h1");
+    introsplitTypes.forEach((char, i) => {
+      const i_text = new SplitType(char);
+      gsap.to(i_text.chars, {
+        y: 0,
+        stagger: 3,
+        duration: 1 
+      });
+    });
+  }, []);
+
   return (
     <div id='home'>
       <div className="bg-EFEAE3 flex justify-center items-center content-container pt-16" style={{ height: '100vh' }}>
         <div className="flex flex-col gap-10">
           <div className="">
             <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-center text-black">
-              <h1 className="text-2xl md:text-5xl font-bold mb-4" style={{ color: 'white', fontSize: '7.5rem' }}>Welcome to AudioBytes</h1>
+              <h1 className="text-2xl md:text-5xl font-bold mb-4" style={{ color: 'white', fontSize: '5rem' }}>Welcome to AudioBytes</h1>
               <h2 className="text-sm md:text-xl" style={{ color: 'white', fontSize: '2rem', marginTop: '20px' }}>MUSIC CLUB OF IIITD</h2>
+              <span className='text-sm md:text-xl'style={{ color: 'white', fontSize: '7rem', marginTop: '20px' }}>   </span>
+              <span className="text" style={{ color: '#f1e728', fontSize: '7rem', marginTop: '20px' , fontWeight: 'bold'}}></span>
             </div>
           </div>
         </div>
-          <video autoPlay loop muted src={video} style={{ height: '100%', width: '100%', objectFit: 'cover' }}></video> 
-          
+        <video autoPlay loop muted src={video} style={{ height: '100%', width: '100%', objectFit: 'cover' }}></video> 
       </div>
     </div>
   );
