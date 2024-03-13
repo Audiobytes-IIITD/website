b8cfed72b2fe4fab8194983cf860e9d840cdcf7d
Anish <132756021+ianishdev@users.noreply.github.com>
2024-03-13 06:57:24 +0530

    Update NavBar.js

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,35 +1,42 @@
 import React from "react";
+import logo from "../images/abWhite300.png";
 
 function NavBar() {
     const [showMenu, setShowMenu] = React.useState(false);
+    
     return (
-        <nav className="flex items-center justify-between bg-slate-800 p-4">
-            <div className="flex items-center">
-                <svg className="w-6 h-6 text-white" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-                    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h16"></path>
-                </svg>
-                <h1 className="ml-4 text-white font-bold text-base">AudioBytes</h1>
-            </div>
-            <div className="md:hidden sm:hi lg:flex items-center">
-                {/* Content for large screens */}
-                <a href="/" className="text-white mr-8 text-sm font-bold">About Us</a>
-                <a href="/" className="text-white mr-8 text-sm font-bold">Upcoming Events</a>
-                <a href="/" className="text-white mr-8 text-sm font-bold">Our Music</a>
-                <a href="/" className="text-white mr-8 text-sm font-bold">Contact</a>
-                <a href="/" className="text-white mr-4 text-sm rounded-full bg-cyan-600 p-3 font-bold">Join the Club!</a>
-            </div>
-            <div className="lg:hidden">
-                {/* Content for small screens */}
-                <button className="text-white text-sm font-bold bg-cyan-600 rounded-full p-3" onClick={() => {setShowMenu(!showMenu); console.log("Hello")}}>Menu</button>
-                <div className={`fixed top-0 left-0 w-full h-full bg-slate-800 z-50 ${showMenu ? "block" : "hidden"}`}>
-                    <button className="text-white text-sm font-bold bg-cyan-600 rounded-full p-3" onClick={() => setShowMenu(false)}>Close</button>
-                    <div className="flex flex-col items-center justify-center h-full">
-                        <a href="/" className="text-white text-2xl font-bold">About Us</a>
-                        <a href="/" className="text-white text-2xl font-bold">Upcoming Events</a>
-                        <a href="/" className="text-white text-2xl font-bold">Our Music</a>
-                        <a href="/" className="text-white text-2xl font-bold">Contact</a>
-                        <a href="/" className="text-white text-2xl font-bold">Join the Club!</a>
-                    </div>
+        <nav className="bg-black dark:bg-gray-900 fixed w-full z-20 top-0 start-0 border-b border-gray-200 dark:border-gray-600">
+            <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+                <div className="flex items-center space-x-3 rtl:space-x-reverse">
+                    <img src={logo} alt="icon" className="w-16 h-16 content-stretch" />
+                    <h1 className="text-2xl font-semibold whitespace-nowrap dark:text-white" style={{ color: 'white' }}>AudioBytes</h1>
+                </div>
+                <div className="flex md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
+                    <button type="button" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Join Us</button>
+                    <button data-collapse-toggle="navbar-sticky" type="button" className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-sticky" aria-expanded="false">
+                        <span className="sr-only">Open main menu</span>
+                        <svg className="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
+                            <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M1 1h15M1 7h15M1 13h15"/>
+                        </svg>
+                    </button>
+                </div>
+
+
+                <div className="items-center justify-between hidden w-full md:flex md:w-auto md:order-1" id="navbar-sticky">
+                    <ul className="flex flex-col p-4 md:p-0 mt-4 font-medium border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-black dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+                        <li>
+                            <a href="#" className="block py-2 px-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 md:dark:text-blue-500" aria-current="page" style={{ color: 'white' }}>Home</a>
+                        </li>
+                        <li>
+                            <a href="#" className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700" style={{ color: 'white' }}>About</a>
+                        </li>
+                        <li>
+                            <a href="#" className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700" style={{ color: 'white' }}>Upcoming Events</a>
+                        </li>
+                        <li>
+                            <a href="#" className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700" >Contact</a>
+                        </li>
+                    </ul>
                 </div>
             </div>
         </nav>
