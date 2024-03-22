1d3bf9923bf5a8ab34b0df9b9b460ff347cb6188
Anish <132756021+ianishdev@users.noreply.github.com>
2024-03-22 09:13:16 +0530

    Update NavBar.js

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -24,6 +24,7 @@ function NavBar() {
                         <Link to='home' activeClass='active' spy={true} smooth={true} offset={-200} style={{cursor:"pointer"}}>
                             <div className="block py-2 px-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 md:dark:text-blue-500" aria-current="page" style={{ color: 'white' }}>Home</div>
                         </Link>
+
                             
                         <Link to='aboutUs' activeClass='active' spy={true} smooth={true} offset={-200} style={{cursor:"pointer"}}>
                             <div className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700" style={{ color: 'white' }}>About</div>
@@ -34,7 +35,11 @@ function NavBar() {
                         </Link>
                         
                         <Link to='joinus' activeClass='active' spy={true} smooth={true} offset={-200} style={{cursor:"pointer"}}>
-                            <div className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"  style={{ color: 'white' }} >Contact</div>
+                            <div className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700" style={{ color: 'white' }} >Contact</div>
+                        </Link>
+
+                        <Link to='room' activeClass='active' spy={true} smooth={true} offset={-200} style={{cursor:"pointer"}}>
+                            <div className="block py-2 px-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 md:dark:text-blue-500" aria-current="page" style={{ color: 'white' }}>Access Music Room</div>
                         </Link>
                     </ul>
                 </div>
