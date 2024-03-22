2f38775838e0ba6f971b54dd39ca0a94992b43c3
Anish <132756021+ianishdev@users.noreply.github.com>
2024-03-22 09:14:39 +0530

    Add files via upload

---
+++ src/pages/Music Room/room.js
@@ -0,0 +1,86 @@
+import React, { useState } from "react";
+import "./room.css";
+
+const Room = () => {
+    const [focused, setFocused] = useState(false);
+
+    return (
+        <div className="room">
+            <div className="room-container">
+                <h1 id="room-title">Access Music Room</h1>
+                
+                <div className="login-box">
+                    <form>
+                        <div className="user-box">
+                            <input
+                                type="text"
+                                name=""
+                                required=""
+                                onFocus={() => setFocused(true)} 
+                                onBlur={(e) => {
+                                    if (e.target.value === '') {
+                                        setFocused(false);
+                                    }
+                                }}
+                            />
+                            <label className={focused ? "label active" : "label"}>Enter your Name</label>
+                        </div>
+                        <div className="user-box">
+                            <input
+                                type="email" 
+                                name=""
+                                required=""
+                                onFocus={() => setFocused(true)} 
+                                onBlur={(e) => {
+                                    if (e.target.value === '') {
+                                        setFocused(false);
+                                    }
+                                }}
+                            />
+                            <label className={focused ? "label active" : "label"}>Enter your IIITD Mail</label>
+                        </div>
+                        <div className="user-box">
+                            <input
+                                type="number" 
+                                name=""
+                                required=""
+                                onFocus={() => setFocused(true)} 
+                                onBlur={(e) => {
+                                    if (e.target.value === '') {
+                                        setFocused(false);
+                                    }
+                                }}
+                            />
+                            <label className={focused ? "label active" : "label"}>Hours</label>
+                        </div>
+                        <div className="user-box" id="date-label">
+                            <input
+                                type="date" 
+                                name=""
+                                required=""
+                                onFocus={() => setFocused(true)} 
+                                onBlur={(e) => {
+                                    if (e.target.value === '') {
+                                        setFocused(false);
+                                    }
+                                }}
+                                placeholder="Select Date"
+                                style={{ color: 'black' }}
+                            />
+                            <label className={focused ? "label active" : "label" } >Date</label>
+                        </div>
+                        <a href="#">
+                            <span></span>
+                            <span></span>
+                            <span></span>
+                            <span></span>
+                            Submit
+                        </a>
+                    </form>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Room;
