04430bd6f20bb1848bbd4d3eb187ee37c8e35abf
Anish <132756021+ianishdev@users.noreply.github.com>
2024-03-31 16:10:24 +0530

    fixed membersc-content for mobile-desktop screens

--- src/components/MemberCard.js
+++ src/components/MemberCard.js
@@ -1,8 +1,15 @@
 import React from "react";
 
 const MemberCard = ({ name, role, email, image }) => {
+    const screenWidth = window.innerWidth;
+
+    let margin = "10px"; 
+    if (screenWidth >= 769 && screenWidth <= 1440) {
+        margin = "100px";
+    }
+
     return (
-        <div className="flex items-center gap-4 bg-black-800 p-4 rounded-xl flex-wrap xs:justify-center sm:justify-center xxs:justify-center" style={{ width: "300px", height: "150px", margin: "10px", flexBasis: "calc(25% - 20px)" }}> {/* Added flexBasis for controlling width */}
+        <div className="flex items-center gap-4 bg-black-800 p-4 rounded-xl flex-wrap xs:justify-center sm:justify-center xxs:justify-center" style={{ width: "300px", height: "150px", margin: margin, flexBasis: "calc(25% - 20px)" }}>
             <img src={image} alt="member" className="w-32 h-32 rounded-full" />
             <div className="flex flex-col gap-2">
                 <h2 className="text-white text-lg font-bold">{name}</h2>
