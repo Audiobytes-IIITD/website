a0bb93c5dd8cd555ef85bd765964f26fa765ce78
Anish <132756021+ianishdev@users.noreply.github.com>
2024-03-31 16:14:25 +0530

    Update MemberCard.js

--- src/components/MemberCard.js
+++ src/components/MemberCard.js
@@ -1,15 +1,8 @@
 import React from "react";
 
 const MemberCard = ({ name, role, email, image }) => {
-    const screenWidth = window.innerWidth;
-
-    let margin = "10px"; 
-    if (screenWidth >= 769 && screenWidth <= 1440) {
-        margin = "100px";
-    }
-
     return (
-        <div className="flex items-center gap-4 bg-black-800 p-4 rounded-xl flex-wrap xs:justify-center sm:justify-center xxs:justify-center" style={{ width: "300px", height: "150px", margin: margin, flexBasis: "calc(25% - 20px)" }}>
+        <div className="flex items-center gap-4 bg-black-800 p-4 rounded-xl flex-wrap xs:justify-center sm:justify-center xxs:justify-center" style={{ width: "300px", height: "150px", margin: "10px", flexBasis: "calc(25% - 20px)" }}> {/* Added flexBasis for controlling width */}
             <img src={image} alt="member" className="w-32 h-32 rounded-full" />
             <div className="flex flex-col gap-2">
                 <h2 className="text-white text-lg font-bold">{name}</h2>
