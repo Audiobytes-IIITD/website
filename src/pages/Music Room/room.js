d4fea9dcc9f4c51f61ed4bbc80635e860e98b487
Anish <132756021+ianishdev@users.noreply.github.com>
2024-03-31 19:39:55 +0530

    Update room.js

--- src/pages/Music Room/room.js
+++ src/pages/Music Room/room.js
@@ -1,83 +1,27 @@
 import React, { useState } from "react";
 import "./room.css";
+import arrow from "../../images/arrow_icon.png"
 
 const Room = () => {
-    const [focused, setFocused] = useState(false);
 
     return (
         <div className="room">
             <div className="room-container">
-                <h1 id="room-title" style={{display:"flex",alignItems:"center"}}>Access Music Room</h1>
-                
-                <div className="login-box">
-                    <form>
-                        <div className="user-box">
-                            <input
-                                type="text"
-                                name=""
-                                required=""
-                                onFocus={() => setFocused(true)} 
-                                onBlur={(e) => {
-                                    if (e.target.value === '') {
-                                        setFocused(false);
-                                    }
-                                }}
-                            />
-                            <label className={focused ? "label active" : "label"}>Enter your Name</label>
-                        </div>
-                        <div className="user-box">
-                            <input
-                                type="email" 
-                                name=""
-                                required=""
-                                onFocus={() => setFocused(true)} 
-                                onBlur={(e) => {
-                                    if (e.target.value === '') {
-                                        setFocused(false);
-                                    }
-                                }}
-                            />
-                            <label className={focused ? "label active" : "label"}>Enter your IIITD Mail</label>
-                        </div>
-                        <div className="user-box">
-                            <input
-                                type="number" 
-                                name=""
-                                required=""
-                                onFocus={() => setFocused(true)} 
-                                onBlur={(e) => {
-                                    if (e.target.value === '') {
-                                        setFocused(false);
-                                    }
-                                }}
-                            />
-                            <label className={focused ? "label active" : "label"}>Hours</label>
-                        </div>
-                        <div className="user-box" id="date-label">
-                            <input
-                                type="date" 
-                                name=""
-                                required=""
-                                onFocus={() => setFocused(true)} 
-                                onBlur={(e) => {
-                                    if (e.target.value === '') {
-                                        setFocused(false);
-                                    }
-                                }}
-                                placeholder="Select Date"
-                                style={{ color: 'black' }}
-                            />
-                            <label className={focused ? "label active" : "label" } >Date</label>
-                        </div>
-                        <a href="#">
-                            <span></span>
-                            <span></span>
-                            <span></span>
-                            <span></span>
-                            Submit
-                        </a>
-                    </form>
-                </div>
+                <form action="https://api.web3forms.com/submit" method="POST" id="form-send">
+                    <div id="room-header" >
+                        <h1 id="room-title">Access Music Room</h1>
+                        <hr />
+                    </div>
+                    <input type="hidden" name="access_key" value="f023b5a7-817f-4a4b-9f22-5f8268513281"></input>
+
+                    <input type="text" name="name" placeholder="Enter name" id="input-form"  required></input>
+                    <input type="email" name="email" placeholder="Enter Your Email" id="input-form" required></input>
+                    <input type="Number" name="hours" placeholder="Hours Required" id="input-form" required></input>
+                    <input type="date" name="date" placeholder="Enter Date" id="input-form" required    ></input>
+
+                    <button type="submit">Submit <img src = {arrow} /></button>
+                    
+                </form>
             </div>
         </div>
     );
