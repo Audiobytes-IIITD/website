5bb8fcd0d88692338f0b3b7fadd05ce4ea4f331a
Anish <132756021+ianishdev@users.noreply.github.com>
2024-04-01 07:37:29 +0530

    made form style consistend

--- src/pages/Music Room/room.js
+++ src/pages/Music Room/room.js
@@ -11,18 +11,25 @@ const Room = () => {
     return (
         <div className="room">
             <div className="room-container">
-                <form action="https://api.web3forms.com/submit" method="POST" id="form-send">
+                <form action="https://api.web3forms.com/submit" method="POST" id="form-send" >
                     <div id="room-header" >
                         <h1 id="room-title">Access Music Room</h1>
                         <hr />
                     </div>
                     <input type="hidden" name="access_key" value="f023b5a7-817f-4a4b-9f22-5f8268513281"></input>
 
-                    <input type="text" name="name" placeholder="Enter name" id="input-form" required></input>
+                    <div className="input-group " id="room-form"> 
+                    <input  type="text" name="name" placeholder="Enter name" id="input-form" required></input>
+                    </div>
+                    <div className="input-group " id="room-form"> 
                     <input type="email" name="email" placeholder="Enter Your Email" id="input-form" required></input>
+                    </div>
+                    <div className="input-group " id="room-form"> 
                     <input type="Number" name="hours" placeholder="Hours Required" id="input-form" required></input>
+                    </div>
+                    <div className="input-group " id="room-form"> 
                     <input type="date" name="date" placeholder="Enter Date" id="input-form" required min={tomorrowFormatted}></input>
-
+                    </div>
                     <button type="submit">Submit <img src={arrow} alt="arrow icon" /></button>
                     
                 </form>
