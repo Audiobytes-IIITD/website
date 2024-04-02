04cd6dac870d65c058107cf704ba83490e72ad5f
Anish <132756021+ianishdev@users.noreply.github.com>
2024-04-02 20:21:19 +0530

    Update Members.js

--- src/pages/Members/Members.js
+++ src/pages/Members/Members.js
@@ -7,7 +7,7 @@ function Members() {
     return (
         <div className="members-container" id="members">
             <h1 className="members-title" id="title">Members Profile</h1>
-            <div className="members-content">
+            <div id="members-content">
                 <MemberCard name="Anish" role="Web Developer" email="anish22075@iiitd.ac.in" image={image} />
                 <MemberCard name="Harsh Aggarwal" role="Web Developer" email="harsh20508@iiitd.ac.in" image={image} />
                 <MemberCard name="Piyush Narula" role="Web Developer" email="piyush22354@iiitd.ac.in" image={image} />
